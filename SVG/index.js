f618c160573a57cf2680caf2ce62fb5c21628407
pantchayan <pantchayan@gmail.com>
2021-10-26 14:09:30 +0530

    initial commit

---
+++ SVG/index.js
@@ -0,0 +1,16 @@
+const a = document.querySelector('div');
+
+const b = document.querySelectorAll('div');
+const c = document.querySelector('.testing');
+
+const a2 = d3.selectAll('div')
+
+console.log("Here")
+
+console.log(a, b, c)
+
+
+
+console.log("D3 : ")
+
+console.log(a2)
