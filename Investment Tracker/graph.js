4fed72a201ccabf86306874a494cd01032946dfe
pantchayan <pantchayan@gmail.com>
2021-10-29 15:48:45 +0530

    Finished Investment Tracker

--- Investment Tracker/graph.js
+++ Investment Tracker/graph.js
@@ -28,6 +28,66 @@ const arcPath = d3
 // ordinal scale
 const colorScale = d3.scaleOrdinal(d3["schemeSet3"]);
 
+let addMouseEvents = () => {
+  graph
+    .selectAll("path")
+    .on("mouseover", handleMouseOver)
+    .on("mouseout", handleMouseOut)
+    .on('click', handleClickEvent);
+};
+
+function handleMouseOver() {
+  let name = d3.select(this)._groups[0][0].__data__.data.name;
+  let cost = d3.select(this)._groups[0][0].__data__.data.cost;
+  let id = d3.select(this)._groups[0][0].__data__.data.id;
+  let posX = window.event.clientX;
+  let posY = window.event.clientY;
+
+  let data = [{ name, cost, id, posX, posY }];
+  makePopUp(data);
+
+  d3.select(this).style("opacity", "0.5");
+}
+
+function handleMouseOut() {
+  makePopUp([]);
+  d3.select(this).style("opacity", "1");
+}
+
+function handleClickEvent() {
+  let id = d3.select(this)._groups[0][0].__data__.data.id;
+  db.collection('investments').doc(id).delete();
+}
+
+let makePopUp = (data) => {
+  let details = svg.selectAll(".detail").data(data);
+  details.exit().remove();
+
+  details
+    .attr("x", (d) => dims.width/2 - 70)
+    .attr("y", (d) => dims.height + 50)
+    .attr("text-anchor", "left")
+    .text((d) => d.name + ": ₹" + d.cost)
+    .style("alignment-baseline", "middle")
+    .style("font-weight", "500")
+    .style("font-size", "15")
+    .style("fill", "white")
+    .attr("class", "detail");
+
+  details
+    .enter()
+    .append("text")
+    .attr("x", 10)
+    .attr("y", (d) => dims.height + 40)
+    .attr("text-anchor", "left")
+    .text((d) => d.name + ": ₹" + d.cost)
+    .style("alignment-baseline", "middle")
+    .style("font-weight", "500")
+    .style("font-size", "15")
+    .style("fill", "white")
+    .attr("class", "detail");
+};
+
 // update function
 let update = (data) => {
   // D3 OPTICS PATTERN
@@ -57,42 +117,65 @@ let update = (data) => {
     .attr("stroke", "#fff")
     .attr("stroke-width", 3)
     .attr("fill", (d) => colorScale(d.data.name))
+    .attr("class", (d) => `${d.data.cost}`)
     .transition()
     .duration(700)
     .attrTween("d", (d) => arcTweenEnter(d));
 
-  texts.exit().remove();
+  texts
+    .exit()
+    // .transition()
+    // .delay((d, i) => 180 * i)
+    // .style("font-size", "0")
+    .remove();
 
   texts
     .attr("y", (d, i) => i * 25) //  25 is the distance between labels
-    .style("fill", d => colorScale(d.data.name))
+    .style("fill", (d) => colorScale(d.data.name))
     .attr("text-anchor", "left")
-    .text(d => d.data.name)
+    .text((d) => d.data.name)
     .style("alignment-baseline", "middle")
-    .style("text-style", "bold")
+    .style("text-style", "bold");
 
   texts
     .enter()
     .append("text")
     .attr("x", 10)
     .attr("y", (d, i) => i * 25) // 100 is where the first dot appears. 25 is the distance between dots
-    .style("fill", d => colorScale(d.data.name))
+    .style("fill", (d) => colorScale(d.data.name))
     .attr("text-anchor", "left")
-    .text(d => d.data.name)
+    .text((d) => d.data.name)
     .style("alignment-baseline", "middle")
     .style("font-weight", "500")
-
-  circles.exit().remove();
+    .style("font-size", "0")
+    .transition()
+    .delay((d, i) => 180 * i)
+    .style("font-size", "15");
 
   circles
-  .enter()
-  .append('circle')
-      .attr("cy", (d, i) => i*25)
-      .style("fill", d => colorScale(d.data.name))
-      .attr("r", 4)
+    .exit()
+    // .transition()
+    // .delay((d, i) => 200 * i)
+    // .attr("r", 0)
+    .remove();
 
+  circles
+    .attr("cy", (d, i) => i * 25)
+    .style("fill", (d) => colorScale(d.data.name))
+    .transition()
+    .delay((d, i) => 200 * i)
+    .attr("r", 4);
 
+  circles
+    .enter()
+    .append("circle")
+    .attr("cy", (d, i) => i * 25)
+    .style("fill", (d) => colorScale(d.data.name))
+    .transition()
+    .delay((d, i) => 200 * i)
+    .attr("r", 4);
 
+  addMouseEvents();
 };
 
 // getting data from firestore in data array
@@ -125,6 +208,7 @@ const arcTweenEnter = (d) => {
   let i = d3.interpolate(d.endAngle, d.startAngle);
 
   return function (t) {
+    // console.table(t , i(t), d.startAngle);
     d.startAngle = i(t);
     return arcPath(d);
   };
