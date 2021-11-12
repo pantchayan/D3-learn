f073fe2c0a1d6ff2a37c9d2ef76abf6398c14815
pantchayan <pantchayan@gmail.com>
2021-11-12 13:34:35 +0530

    Football game

--- SVG Animations/Football/index.js
+++ SVG Animations/Football/index.js
@@ -1,3 +1,4 @@
+let totalCollisions = 0;
 let container = d3.select("#container");
 
 let containerHeight = parseInt(container.style("height"));
@@ -5,8 +6,13 @@ let containerWidth = parseInt(container.style("width"));
 
 let playground = d3.select("#container").append("svg").attr("id", "playground");
 
-let makeSquares = () => {
-  setInterval(() => {
+let gameOver = false;
+let currScore = 0;
+// makes 3 squares @ every 2.5 seconds
+// transitions from top to bottom
+let makeObjects = () => {
+  let makingObject = setInterval(() => {
+    if (gameOver) clearInterval(makingObject);
     let num = Math.random() * containerWidth;
     playground.select("rect").remove();
 
@@ -14,12 +20,13 @@ let makeSquares = () => {
       .append("rect")
       .attr("x", num)
       .attr("y", -100)
+      .attr("class", "object")
       .attr("width", 100)
       .attr("height", 100)
       .transition()
       .ease(d3.easeLinear)
-      .duration(1000)
-      .attr("y", containerHeight);
+      .duration(2500)
+      .attr("y", parseInt(container.style("height")));
 
     // playground.select("rect").remove();
 
@@ -28,16 +35,91 @@ let makeSquares = () => {
       .append("rect")
       .attr("x", num)
       .attr("y", -100)
+      .attr("class", "object")
       .attr("width", 100)
       .attr("height", 100)
       .transition()
-      .delay(500)
+      .delay(700)
       .ease(d3.easeLinear)
-      .duration(1000)
-      .attr("y", containerHeight);
-  }, 2000);
+      .duration(2500)
+      .attr("y", parseInt(container.style("height")));
+
+    num = Math.random() * containerWidth;
+    playground
+      .append("rect")
+      .attr("x", num)
+      .attr("y", -100)
+      .attr("class", "object")
+      .attr("width", 100)
+      .attr("height", 100)
+      .transition()
+      .delay(1500)
+      .ease(d3.easeLinear)
+      .duration(2500)
+      .attr("y", parseInt(container.style("height")));
+  }, 2500);
 };
 
-console.log(containerHeight, containerWidth);
+// checks collision between ball and objects on playground
+// returns true if collision occurs
+let checkCollision = () => {
+  let footballSVG = document.querySelector("#football>svg");
+  let objectsSVG = document.querySelectorAll(".object");
 
-makeSquares();
+  let footballR = footballSVG.getBoundingClientRect();
+  let flag = false;
+  for (let i = 0; i < objectsSVG.length; i++) {
+    let objR = objectsSVG[i].getBoundingClientRect();
+    flag = !(
+      objR.left > footballR.right-10 ||
+      objR.right < footballR.left+10 ||
+      objR.top > footballR.bottom-10 ||
+      objR.bottom < footballR.top+10
+    );
+
+    if (flag) {
+      break;
+    }
+  }
+
+  return flag;
+};
+
+// checks if a collision occurs at every 0.01 second.
+let runGame = () => {
+  let gameLoop = setInterval(() => {
+    // flag == true if collision occurs
+    gameOver = checkCollision();
+    if (gameOver) {
+      console.log(`Collision detected`);
+      alert("GAME OVER");
+      newGame();
+      clearInterval(gameLoop);
+    }
+    currScore += 3;
+    document.getElementById(
+      "curr-score"
+    ).innerHTML = `Curr score : <span> ${currScore} </span>`;
+  }, 300);
+};
+
+window.addEventListener("resize", () => {
+  // let containerHeight = parseInt(container.style("height"));
+  containerWidth = parseInt(container.style("width"));
+});
+
+let updateMaxScore = () => {
+  
+  let maxS = document.getElementById("max-score");
+  maxS.innerHTML = `Max score : <span> ${currScore} </span>`;
+};
+
+let newGame = () => {
+  updateMaxScore();
+  playground.selectAll("rect").remove();
+
+  makeObjects();
+  runGame();
+};
+
+// newGame();
